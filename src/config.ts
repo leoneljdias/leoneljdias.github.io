937eb8e8cd931e5bb88bf0cc748c7a1fb8266380
Leonel Dias <leonel.j.dias@gmail.com>
2025-03-29 17:58:50 +0000

    Increase post limits for index and page configurations

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,8 @@ export const SITE = {
   title: "Leonel Dias",
   ogImage: "me.jpg",
   lightAndDarkMode: true,
-  postPerIndex: 4,
-  postPerPage: 4,
+  postPerIndex: 5,
+  postPerPage: 5,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
   showBackButton: true, // show back button in post detail
